95fe6575fbc63b10846305722fb4211eec4042b5
HauHauHazuMeuHazuTau <83718692+Nowah72@users.noreply.github.com>
2025-02-22 22:09:30 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,16 @@
 
 # Install system-level dependencies
 apt-get update
-apt-get install -y build-essential ta-lib
+apt-get install -y build-essential wget
+
+# Download and install ta-lib
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xzf ta-lib-0.4.0-src.tar.gz
+cd ta-lib
+./configure --prefix=/usr
+make
+make install
+cd ..
 
 # Install Python dependencies
 pip install -r requirements.txt
